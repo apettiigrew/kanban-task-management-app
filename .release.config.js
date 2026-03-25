d453cca777ea4ecf2a05dc6c4c03b9a6406bb916
Andrew Pettigrew <36255731+apettiigrew@users.noreply.github.com>
2026-03-25 13:31:04 -0500

    chore: udpate release config (#35)

--- .release.config.js
+++ .release.config.js
@@ -1,16 +1,25 @@
 module.exports = {
-    branches: ["main"],
-    plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+
+    [
       "@semantic-release/npm",
-      [
-        "@semantic-release/git",
-        {
-          assets: ["package.json"],
-          message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-        },
-      ],
-      "@semantic-release/github",
+      {
+        npmPublish: false, // if you're NOT publishing to npm
+      },
     ],
-  };
+
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "package-lock.json"], // IMPORTANT
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+
+    "@semantic-release/github",
+  ],
+};
